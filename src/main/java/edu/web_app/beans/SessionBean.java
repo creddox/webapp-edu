e35995a872c7c0ec59dd9ac3f4cb88464e9d8b60
Christian Schaum <ceschaum@googlemail.com>
2013-02-25 16:30:38 +0100

    Added simple implementation of session object (SessionBean).

---
+++ src/main/java/edu/web_app/beans/SessionBean.java
@@ -0,0 +1,23 @@
+package edu.web_app.beans;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+
+public class SessionBean {
+	private Date creationDate;
+	private UUID sessionKey;
+	private ServletContext context;
+	
+	public SessionBean() {
+		this.creationDate = new Date();
+	}
+
+	public UUID getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(UUID sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+}
