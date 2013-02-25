39ed894d5954bb57966077c2fbbc450f4e4d4c70
Christian Schaum <ceschaum@googlemail.com>
2013-02-25 12:11:16 +0100

    Added login interface + logic for simple output of table.

--- src/main/java/edu/web_app/LoginServlet.java
+++ src/main/java/edu/web_app/LoginServlet.java
@@ -2,11 +2,19 @@ package edu.web_app;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 
 public class LoginServlet extends HttpServlet {
@@ -35,10 +43,49 @@ public class LoginServlet extends HttpServlet {
 		
 		String password = "";
 		password = request.getParameter("password");
+		
+		this.doQuery(loginName, password, out);
 	}
 	
-	protected void doQuery(String query) {
+	//Checks the user credentials and writes them in the session context. Returns true, if
+	//user has provided valid credentials and returns false, if user has not.
+	protected boolean doQuery(String loginName, String password, PrintWriter out) {
+		InitialContext appContext;
+		boolean returnValue = false;
 		
+		try {
+			//Initializing the connection
+			appContext = new InitialContext();
+			DataSource ds = (DataSource) appContext.lookup("java:comp/env/jdbc/UserDB");			
+			Connection conn = ds.getConnection();
+			
+			//Building query
+			String queryString = "";
+			queryString = "SELECT * FROM app.users WHERE user_name= ? AND password = ? ";
+			PreparedStatement query = conn.prepareStatement(queryString);
+			
+			//Inserting values into query
+			query.setString(1, loginName);
+			query.setString(2, password);
+			
+			//Execute statement
+			ResultSet rs = query.executeQuery();
+			
+			while (rs.next()) {
+				out.println("UID: " + rs.getInt(1));
+				out.println("Username: " + rs.getString(2));
+				out.println("Password: " + rs.getString(3));
+			}
+			
+			rs.close();
+			query.close();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return returnValue;
 	}
 
 }
